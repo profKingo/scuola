7dc706b926f04ce82637831793a574c7ea30e04e
profKingo <profgciccolone@gmail.com>
2024-08-21 07:37:28 +0200

    creato contoCorrente

--- TPS 4/contoCorrente/src/contocorrente/ContoCorrente.java
+++ TPS 4/contoCorrente/src/contocorrente/ContoCorrente.java
@@ -16,6 +16,35 @@ public class ContoCorrente {
     /**
      * @param args the command line arguments
      */
+    public ContoCorrente(){
+        nome="";
+        numconto=9999;
+        saldo=0;
+    }
+    public ContoCorrente(String n, int c, int s){
+        nome=n;
+        numconto=c;
+        saldo=s;
+    }
+
+    public void versa(int s){
+        saldo+=s;
+    }
+
+    public void preleva(int s){
+        if (saldo>=s){
+            saldo-=s;
+        }
+        else{
+            System.out.println("Saldo non sufficiente! puoi prelevare fino a: " + saldo);
+            saldo=0;
+        }
+    }
+    void stampaConto(){
+        System.out.println("Conto       " + numconto);
+        System.out.println("Intestato a " + nome);
+        System.out.println("Saldo conto " + saldo);
+    }
     public static void main(String[] args) {
         // TODO code application logic here
     }
